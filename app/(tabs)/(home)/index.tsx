0ac7e2dc3d100e97c132ef1609513fc10846dd53
sanghun lee <dyhong9212@gmail.com>
2025-12-07 18:18:11 +0900

    feat: 탭 바 애니메이션 추가

--- app/(tabs)/(home)/index.tsx
+++ app/(tabs)/(home)/index.tsx
@@ -1,26 +1,46 @@
-import { useRouter } from "expo-router"
-import { Text, TouchableOpacity, View } from "react-native"
-
+import { BlurView } from 'expo-blur';
+import { usePathname, useRouter } from "expo-router";
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 1
 export default function Index() {
     const router = useRouter();
+    const pathname = usePathname();
+    const insets = useSafeAreaInsets();
+    const isLoggedIn = false;
 
     return (
-        <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-            <View>
-                <TouchableOpacity
-                    onPress={() => router.push(`/`)}
-                >
-                    <Text>For you</Text>
-                </TouchableOpacity>
-            </View>
-            <View>
-                <TouchableOpacity
-                    onPress={() => router.push(`/following`)}
-                >
-                    <Text>Following</Text>
-                </TouchableOpacity>
-            </View>
+        <View style={[style.container, { paddingTop: insets.top + 20 }]}>
+            <BlurView style={style.header} intensity={70}>
+                <Image
+                    source={require("../../../assets/images/react-logo.png")}
+                    style={style.headerLogo}
+                />
+                {!isLoggedIn &&
+                    <TouchableOpacity
+                        onPress={() => router.navigate(`/login`)}
+                        style={style.loginButton}
+                    >
+                        <Text style={style.loginButtonText}>로그인</Text>
+                    </TouchableOpacity>
+                }
+            </BlurView>
+            {isLoggedIn && (
+                <View style={style.tabContainer}>
+                    <View style={style.tab}>
+                        <TouchableOpacity
+                            onPress={() => router.push(`/`)}>
+                            <Text style={{ color: pathname === "/" ? "red" : "black" }}>For you</Text>
+                        </TouchableOpacity>
+                    </View>
+                    <View style={style.tab}>
+                        <TouchableOpacity
+                            onPress={() => router.push(`/following`)}>
+                            <Text style={{ color: pathname === "/" ? "black" : "red" }}>Following</Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+            )}
             <View>
                 <TouchableOpacity
                     onPress={() => router.push(`/@dayoung/post/1`)}
@@ -45,3 +65,38 @@ export default function Index() {
         </View>
     )
 }
+
+const style = StyleSheet.create({
+    container: {
+        flex: 1
+    },
+    tabContainer: {
+        flexDirection: "row"
+    },
+    tab: {
+        flex: 1,
+        alignItems: "center"
+    },
+    header: {
+        alignItems: "center",
+    },
+    headerLogo: {
+        width: 42,
+        height: 42
+    },
+    loginButton: {
+        position: "absolute",
+        right: 10,
+        top: 0,
+        backgroundColor: "black",
+        borderWidth: 1,
+        borderColor: "black",
+        paddingVertical: 10,
+        paddingHorizontal: 20,
+        borderRadius: 10,
+    },
+    loginButtonText: {
+        color: "white",
+
+    }
+})
--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,93 +1,191 @@
 import { Ionicons } from "@expo/vector-icons";
+import { type BottomTabBarButtonProps } from "@react-navigation/bottom-tabs";
 import { Tabs, useRouter } from "expo-router";
+import React, { useRef, useState } from "react";
+import { Animated, Modal, Pressable, Text, TouchableOpacity, View } from "react-native";
+
+const AnimatedTabBarButton = ({
+    children,
+    onPress,
+    style,
+    ...restProps
+}: BottomTabBarButtonProps) => {
+    const scaleValue = useRef(new Animated.Value(1)).current;
+
+    const handlePressOut = () => {
+        Animated.sequence([
+            Animated.spring(scaleValue, {
+                toValue: 2,
+                useNativeDriver: true,
+                speed: 200,
+            }),
+            Animated.spring(scaleValue, {
+                toValue: 1,
+                useNativeDriver: true,
+                speed: 200,
+            })
+        ]).start()
+    }
+
+    const { ref: _ref, ...pressableProps } = restProps;
+
+    return (
+        <Pressable
+            {...pressableProps}
+            onPress={onPress}
+            onPressOut={handlePressOut}
+            style={[
+                { flex: 1, justifyContent: "center", alignItems: "center" },
+                style
+            ]}
+            android_ripple={{ borderless: false, radius: 0 }}
+        >
+            <Animated.View style={{ transform: [{ scale: scaleValue }] }}>
+                {children}
+            </Animated.View>
+        </Pressable>
+    )
+}
 
 export default function TabsLayout() {
     const router = useRouter();
+    const isLoggedIn = true;
+    const [isLoginModalOpen, setIsLoginModalOpen] = useState(false);
+
+    const openLoginModal = () => {
+        setIsLoginModalOpen(true);
+    }
+
+    const closeLoginModal = () => {
+        setIsLoginModalOpen(false);
+    }
 
     return (
-        <Tabs
-            screenOptions={{
-                headerShown: false
-            }}
-        >
-            <Tabs.Screen
-                name="(home)"
-                options={{
-                    tabBarLabel: () => null,
-                    tabBarIcon: ({ focused }) => (
-                        <Ionicons
-                            name="home"
-                            size={24}
-                            color={focused ? "black" : "gray"}
-                        />
-                    )
+        <>
+            <Tabs
+                backBehavior="history"
+                screenOptions={{
+                    headerShown: false,
+                    tabBarButton: (props) => <AnimatedTabBarButton {...props} />,
                 }}
-            />
-            <Tabs.Screen
-                name="search"
-                options={{
-                    tabBarLabel: () => null,
-                    tabBarIcon: ({ focused }) => (
-                        <Ionicons
-                            name="search"
-                            size={24}
-                            color={focused ? "black" : "gray"}
-                        />
-                    )
-                }}
-            />
-            <Tabs.Screen
-                name="add"
-                listeners={{
-                    tabPress: (e) => {
-                        e.preventDefault();
-                        router.navigate("/modal")
-                    }
-                }}
-                options={{
-                    tabBarLabel: () => null,
-                    tabBarIcon: ({ focused }) => (
-                        <Ionicons
-                            name="add"
-                            size={24}
-                            color={focused ? "black" : "gray"}
-                        />
-                    )
-                }}
-            />
-            <Tabs.Screen
-                name="activity"
-                options={{
-                    tabBarLabel: () => null,
-                    tabBarIcon: ({ focused }) => (
-                        <Ionicons
-                            name="heart-outline"
-                            size={24}
-                            color={focused ? "black" : "gray"}
-                        />
-                    )
-                }}
-            />
-            <Tabs.Screen
-                name="[username]"
-                options={{
-                    title: "Home",
-                    tabBarLabel: () => null,
-                    tabBarIcon: ({ focused }) => (
-                        <Ionicons
-                            name="person-outline"
-                            size={24}
-                            color={focused ? "black" : "gray"}
-                        />
-                    )
-                }}
-            />
-            <Tabs.Screen
-                name="(post)/[username]/post/[postID]"
-                options={{
-                    href: null,
-                }}
-            />
-        </Tabs>
+            >
+                <Tabs.Screen
+                    name="(home)"
+                    options={{
+                        tabBarLabel: () => null,
+                        tabBarIcon: ({ focused }) => (
+                            <Ionicons
+                                name="home"
+                                size={24}
+                                color={focused ? "black" : "gray"}
+                            />
+                        )
+                    }}
+                />
+                <Tabs.Screen
+                    name="search"
+                    options={{
+                        tabBarLabel: () => null,
+                        tabBarIcon: ({ focused }) => (
+                            <Ionicons
+                                name="search"
+                                size={24}
+                                color={focused ? "black" : "gray"}
+                            />
+                        )
+                    }}
+                />
+                <Tabs.Screen
+                    name="add"
+                    listeners={{
+                        tabPress: (e) => {
+                            e.preventDefault();
+                            if (isLoggedIn) {
+                                router.navigate("/modal")
+                            } else {
+                                openLoginModal();
+                            }
+                        }
+                    }}
+                    options={{
+                        tabBarLabel: () => null,
+                        tabBarIcon: ({ focused }) => (
+                            <Ionicons
+                                name="add"
+                                size={24}
+                                color={focused ? "black" : "gray"}
+                            />
+                        )
+                    }}
+                />
+                <Tabs.Screen
+                    name="activity"
+                    listeners={{
+                        tabPress: (e) => {
+                            if (!isLoggedIn) {
+                                e.preventDefault();
+                                openLoginModal();
+                            }
+                        }
+                    }}
+                    options={{
+                        tabBarLabel: () => null,
+                        tabBarIcon: ({ focused }) => (
+                            <Ionicons
+                                name="heart-outline"
+                                size={24}
+                                color={focused ? "black" : "gray"}
+                            />
+                        )
+                    }}
+                />
+                <Tabs.Screen
+                    name="[username]"
+                    listeners={{
+                        tabPress: (e) => {
+                            if (!isLoggedIn) {
+                                e.preventDefault();
+                                openLoginModal();
+                            }
+                        }
+                    }}
+                    options={{
+                        title: "Home",
+                        tabBarLabel: () => null,
+                        tabBarIcon: ({ focused }) => (
+                            <Ionicons
+                                name="person-outline"
+                                size={24}
+                                color={focused ? "black" : "gray"}
+                            />
+                        )
+                    }}
+                />
+                <Tabs.Screen
+                    name="(post)/[username]/post/[postID]"
+                    options={{
+                        href: null,
+                    }}
+                />
+            </Tabs>
+            <Modal
+                visible={isLoginModalOpen}
+                transparent={true}
+                animationType="fade"
+            >
+                <View style={{
+                    flex: 1,
+                    justifyContent: "flex-end",
+                    backgroundColor: "rgba(0,0,0,0.5)"
+                }}>
+                    <View style={{ backgroundColor: "white", padding: 20 }}>
+                        <Text>Login Modal</Text>
+                        <TouchableOpacity onPress={closeLoginModal}>
+                            <Ionicons name="close" size={24} color="#555" />
+                        </TouchableOpacity>
+                    </View>
+                </View>
+            </Modal>
+        </>
     )
 }
